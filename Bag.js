47418b3ac1eeb44ade3af9d50639f04d1770e4d7
beyenesura <123514564+beyenesura@users.noreply.github.com>
2023-02-21 15:27:42 -0500

    Update Bag.js

--- Bag.js
+++ Bag.js
@@ -1,8 +1,35 @@
 class Bag {
-    // Include constructor Method with the following property: 
-        // 1. Weight
-            //1a. Should handle an exception in case user adds no weight and throw an error.
-  
+  constructor(items = [], weight) {
+    if (weight === undefined) {
+      throw new Error("Bag needs weight");
+    }
+    this.items = items;
+    this.weight = weight;
+  }
+
+  add(item, weight) {
+    this.items.push(item);
+    this.weight += weight;
+  }
+
+  remove(item, weight) {
+    const index = this.items.indexOf(item);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+      this.weight -= weight;
+    }
+  }
+
+  count(item) {
+    let count = 0;
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i] === item) {
+        count++;
+      }
+    }
+    return count;
+  }
 }
 
-//Export the module
+module.exports = Bag;
+
